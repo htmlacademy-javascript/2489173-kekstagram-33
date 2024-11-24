b58d1363b58f919232c0e148b39b9ea222b22eb7
Keks <keks@htmlacademy.ru>
2024-11-24 15:21:40 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/form.js
@@ -0,0 +1,103 @@
+import { resetScale } from './scale-image';
+import { resetEffects } from './effect-image';
+
+
+const form = document.querySelector('.img-upload__form');
+const overlay = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const cancelButton = document.querySelector('#upload-cancel');
+const fileField = document.querySelector('#upload-file');
+const hashtagField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
+
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error',
+});
+
+//Открытие и закрытие формы
+const showModalWindow = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onEscKeyDown);
+};
+
+const hideModalWindow = () => {
+  form.reset();
+  pristine.reset();
+  resetScale();
+  resetEffects();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscKeyDown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+  document.activeElement === commentField;
+
+function onEscKeyDown(evt) {
+  if (evt.key === 'Escape' && !isTextFieldFocused()) {
+    evt.preventDefault();
+    hideModalWindow();
+  }
+}
+
+//Валидации формы
+const MAX_HASHTAG_COUNT = 5;
+const MIN_HASHTAG_LENGTH = 2;
+const MAX_HASHTAG_LENGTH = 20;
+const UNVALID_SYMBOLS = /[^a-zA-Z0-9а-яА-ЯёЁ]/g;
+
+
+const startsWithHash = (string) => string[0] === '#';
+
+const hasValidLength = (string) =>
+  string.length >= MIN_HASHTAG_LENGTH && string.length <= MAX_HASHTAG_LENGTH;
+
+const hasValidSymbols = (string) => !UNVALID_SYMBOLS.test(string.slice(1));
+
+const isValidTag = (tag) =>
+  startsWithHash(tag) && hasValidLength(tag) && hasValidSymbols(tag);
+
+const hasValidCount = (tags) => tags.length <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+const validateTags = (value) => {
+  const tags = value
+    .trim()
+    .split(' ')
+    .filter((tag) => tag.trim().length);
+  return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
+};
+
+pristine.addValidator(
+  hashtagField,
+  validateTags,
+  'Неправильно заполнены хэштеги'
+);
+
+const maxСommentFieldLength = 140;
+const validateComment = (value) => value.length < maxСommentFieldLength;
+
+pristine.addValidator(
+  commentField,
+  validateComment,
+  `Длина комментария не должна быть больше ${maxСommentFieldLength} символов`
+);
+
+// const onFormSubmit = (evt) => {
+//   evt.preventDefault();
+//   pristine.validate();
+// };
+
+fileField.addEventListener('change', showModalWindow);
+cancelButton.addEventListener('click', hideModalWindow);
+form.addEventListener('submit', pristine.validate);
+// form.addEventListener('submit', onFormSubmit());
